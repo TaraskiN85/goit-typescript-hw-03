28d882a7eb8d4ededbb40a5ad380b88acb7b06d4
TaraskiN85 <taras12304@i.ua>
2024-02-19 18:46:23 +0200

    accomplish task 3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,69 @@
-const key = new Key();
+interface IKey {
+  signature: number;
+  getSignature: () => void;
+}
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Key implements IKey {
+  signature: number;
 
-house.openDoor(person.getKey());
+  constructor() {
+    this.signature = this.getSignature()
+  }
+  
+  getSignature = () => {
+    return Math.random()
+  }
+}
 
-house.comeIn(person);
 
+class Person {
+  key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[];
+
+  constructor(protected key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Add tenant");
+    } else {
+      console.log("Nobody home");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.signature === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is opened");
+    } else {
+      console.log("Door is closed");
+    }
+  }
+}
 
 export {};
